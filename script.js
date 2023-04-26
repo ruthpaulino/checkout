b51d292de857892f303b8bb416be3875c6790cbe
Ruth Paulino <ruthemilly@gmail.com>
2023-04-25 23:44:17 -0300

    modified

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function getValue(){
+function getValue() {
 
     var preco = 69.90;
     var value = document.getElementById("qtd").value;
     var total = preco * value;
-    
-    document.getElementById("result").innerHTML= "O Total é" + " " + total;
+
+    document.getElementById("result").innerHTML = "O Total é" + " " + total;
 
 
 };
